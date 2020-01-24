93952e14d9b3fef22966783705fb57b721a6e207
Kendra Marable <60274624+kendramarable@users.noreply.github.com>
2020-01-24 17:19:43 -0500

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,26 @@
+// Kendra Marable
+// CSE 02 Assignment 1
+// January 24, 2020
+// 
+public class Assignment1{
+  public static void main(String args[]){
+    // prints trees to terminal window
+    // each line corresponds to a line of the trees
+    // note the spaces created to make them line up correctly
+    // double backslash to fix the single backslash problem
+    // java thinks of "\" as an operator!
+    System.out.println("    ^        *     ^     *      *  ^");
+    System.out.println(" * /#\\            /#\\             /#\\ *");
+    System.out.println("  /#|#\\ *        /#|#\\    *      /#|#\\");
+    System.out.println(" /##|##\\    *   /##|##\\ *       /##|##\\");
+    System.out.println("  /#|#\\  *     * /#|#\\       *   /#|#\\");
+    System.out.println(" /##|##\\        /##|##\\     *   /##|##\\ *");
+    System.out.println("/###|###\\    * /###|###\\       /###|###\\");
+    System.out.println(" *  |     *        |       *   *   |");
+    System.out.println(" *****************************************");
+    System.out.println(" ************ it's snowing! :) ***********");
+    System.out.println(" *****************************************");
+    
+  }
+  
+}
